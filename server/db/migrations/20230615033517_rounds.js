35c218a1ac6cd4cdf236f43043c42eb07abafde8
Josh Liuaana <jfliuaana@gmail.com>
2023-06-15 15:53:13 +1200

    database: create rounds table

---
+++ server/db/migrations/20230615033517_rounds.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('rounds', (table) => {
+    table.increments('id')
+    table.integer('course_id')
+    table.string('golfer_id')
+    table.text('putts')
+    table.text('gir')
+    table.text('fir')
+    table.text('score')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('rounds')
+}
