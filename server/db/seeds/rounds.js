6be04c252763448ed8bca8dc83d2283b03cb9036
Josh Liuaana <jfliuaana@gmail.com>
2023-06-16 15:30:03 +1200

    realistic seed data

--- server/db/seeds/rounds.js
+++ server/db/seeds/rounds.js
@@ -8,7 +8,7 @@ exports.seed = async function (knex) {
       golfer_id: '7fe67614-2735-4b0c-8de5-f8cf3c303397',
       putts: '1,2,2,1,3,2,2,1,2,1,1,2,2,3,2,1,1,2',
       gir: 'true,true,false,true,false,true,false,true,true,true,true,false,true,false,true,false,true,true',
-      fir: 'true,true,true,true,false,false,true,false,true,true,true,true,false,true,true,false,true,false',
+      fir: 'true,true,false,false,false,false,false,false,false,true,true,false,false,true,true,false,true,false',
       gross: '3,4,3,4,3,4,5,5,4,3,4,5,5,6,6,5,5,6',
     },
     {
@@ -17,7 +17,7 @@ exports.seed = async function (knex) {
       golfer_id: '7fe67614-2735-4b0c-8de5-f8cf3c303397',
       putts: '1,2,1,2,1,2,2,1,3,2,3,2,1,1,1,2,2,2',
       gir: 'true,false,true,true,true,true,false,true,false,true,false,true,true,true,true,false,true,false',
-      fir: 'true,true,true,true,true,false,false,true,false,true,false,true,true,true,true,false,true,false',
+      fir: 'true,true,false,false,true,false,false,true,false,true,false,false,false,true,true,false,true,false',
       gross: '5,4,5,3,6,3,6,6,5,3,4,3,4,5,4,3,4,5',
     },
   ])
