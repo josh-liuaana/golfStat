ceb50f38ccfa643d0eb31270fb935327afd9e3a7
Josh Liuaana <jfliuaana@gmail.com>
2023-06-16 14:42:00 +1200

    single round api

--- client/apis/rounds.ts
+++ client/apis/rounds.ts
@@ -4,3 +4,8 @@ export async function fetchRounds() {
   const res = await request.get('/api/v1/rounds')
   return res.body
 }
+
+export async function fetchSingleRound(id: number) {
+  const res = await request.get(`/api/v1/rounds/${id}`)
+  return res.body
+}
