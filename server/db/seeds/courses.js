633038d8946d1543092537204f5b5015890474fb
Josh Liuaana <jfliuaana@gmail.com>
2023-06-22 10:47:33 +1200

    Add more course seed data

--- server/db/seeds/courses.js
+++ server/db/seeds/courses.js
@@ -10,5 +10,37 @@ exports.seed = async function (knex) {
       par: 66,
       par_per_hole: '4,4,3,3,5,4,3,4,3,4,4,3,3,5,4,3,4,3',
     },
+    {
+      id: 2,
+      name: 'Shandon',
+      distance: 5477,
+      slope: 117,
+      par: 70,
+      par_per_hole: '5,4,3,4,4,4,3,5,3,4,3,4,4,3,5,4,4,4',
+    },
+    {
+      id: 3,
+      name: 'Wanaka',
+      distance: 5484,
+      slope: 122,
+      par: 70,
+      par_per_hole: '4,3,4,4,4,3,4,4,4,5,4,4,3,4,3,5,4,4',
+    },
+    {
+      id: 4,
+      name: 'Te Marua',
+      distance: 5570,
+      slope: 117,
+      par: 71,
+      par_per_hole: '4,3,5,4,4,3,5,4,3,5,3,4,4,5,3,4,4,4',
+    },
+    {
+      id: 5,
+      name: 'Wainuiomata',
+      distance: 5489,
+      slope: 120,
+      par: 72,
+      par_per_hole: '4,3,4,4,5,4,5,4,3,4,3,5,3,5,4,4,4,4',
+    },
   ])
 }
