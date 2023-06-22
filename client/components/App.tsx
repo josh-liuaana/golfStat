4cc865c96cc9094caafe0296531369e78736c1b7
Josh Liuaana <jfliuaana@gmail.com>
2023-06-23 10:24:22 +1200

    Dispatch and format

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,16 +1,16 @@
-import { useEffect } from "react"
-import { Outlet } from "react-router-dom"
+import { useEffect } from 'react'
+import { Outlet } from 'react-router-dom'
 import * as actions from '../actions/rounds'
 import * as courseActions from '../actions/courses'
-import { useAppDispatch } from "../hooks/redux"
+import { useAppDispatch } from '../hooks/redux'
 
 function App() {
   const dispatch = useAppDispatch()
   useEffect(() => {
     dispatch(actions.getRounds())
     dispatch(courseActions.getCourses())
-  }, [])
-  
+  }, [dispatch])
+
   return (
     <>
       <p>App Component</p>
