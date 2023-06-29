3a494993faed677d93efca851c29e0ac326cca6b
Josh Liuaana <jfliuaana@gmail.com>
2023-06-29 16:13:11 +1200

    home component features

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,12 +1,47 @@
-import AddRound from "./AddRound"
+import { useState } from 'react'
+
+import AddRound from './AddRound'
 
 function Home() {
-  
+  const [showRoundSelection, setShowRoundSelection] = useState(false)
   return (
-    <>
-      <p>Home Component</p>
-      <AddRound />
-    </>
+    <div className="home-container">
+      <h1>Josh Liua&apos;ana</h1>
+      <img src="../../images/golf.png" alt="gS logo" />
+      {showRoundSelection ? (
+        <>
+          <button onClick={() => setShowRoundSelection(!showRoundSelection)}>
+            Go back
+          </button>
+          <AddRound />
+        </>
+      ) : (
+        <div className="button-container">
+          <div className="top-buttons-container">
+            <button
+              className="home-buttons stat-buttons"
+              onClick={() => alert('navigate to round history page')}
+            >
+              Playing History
+            </button>
+            <button
+              className="home-buttons stat-buttons"
+              onClick={() => alert('navigate to statistics page')}
+            >
+              Statistics
+            </button>
+          </div>
+          <div>
+            <button
+              className="home-buttons new-round-button"
+              onClick={() => setShowRoundSelection(!showRoundSelection)}
+            >
+              New Round
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
