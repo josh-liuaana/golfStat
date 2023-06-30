8a4629ba548fb92c96eba906b52823ae53da9210
Josh Liuaana <jfliuaana@gmail.com>
2023-06-30 15:18:34 +1200

    current round styling

--- client/components/CurrentRound.tsx
+++ client/components/CurrentRound.tsx
@@ -3,8 +3,10 @@ import { CourseState, CurrentData } from '../../models/types'
 import { useAppSelector, useAppDispatch } from '../hooks/redux'
 import { setScore } from '../actions/courses'
 import { addRound } from '../actions/rounds'
+import { FaArrowRight, FaArrowLeft } from 'react-icons/fa'
 
 // TODO Figure out checkbox bug
+// TODO Putt and gross is required field
 // * Will it require additional function? cant un-check
 
 function CurrentRound() {
@@ -77,56 +79,115 @@ function CurrentRound() {
 
   return (
     <>
-      <p>
-        {courses.current.course.name} - {holeLength} hole round
-      </p>
-      <button onClick={() => handleClick(-1)} disabled={previousDisabled}>
-        Previous Hole
-      </button>
-      <p>Hole: {currentHole}</p>
-      <p>Par: {courses.current.course.parPerHole[currentHole - 1]}</p>
-      <button onClick={() => handleClick(1)} disabled={nextDisabled}>
-        Next Hole
-      </button>
-      <form onSubmit={handleSubmit}>
-        {/* CHECKBOX GIR FIR */}
-        <label htmlFor="fir">Fairway</label>
+      <div className="round-information">
+        <p>
+          {courses.current.course.name} ({holeLength})
+        </p>
+        <p>Gross: -</p>
+        <p>Score: -</p>
+        <p>Putt avg:</p>
+      </div>
 
-        <input
-          id="fir"
-          name="fir"
-          type="checkbox"
-          disabled={courses.current.course.parPerHole[currentHole - 1] === 3}
-          onChange={handleBoolChange}
-          checked={courses.current.currentData.fir[currentHole - 1]} // true/false
-        />
-        <label htmlFor="gir">Green</label>
-        <input
-          id="gir"
-          name="gir"
-          type="checkbox"
-          onChange={handleBoolChange}
-          checked={courses.current.currentData.gir[currentHole - 1]}
-        />
-        {/* NUMBER PUTTS GROSS */}
-        <label htmlFor="gross">Gross</label>
-        <input
-          id="gross"
-          name="gross"
-          type="number"
-          onChange={handleNumChange}
-          value={courses.current.currentData.gross[currentHole - 1]}
-        />
-        <label htmlFor="putts">Putts</label>
-        <input
-          id="putts"
-          name="putts"
-          type="number"
-          onChange={handleNumChange}
-          value={courses.current.currentData.putts[currentHole - 1]}
-        />
-        {currentHole === holeLength && <input type="submit" value="Submit" />}
-        <input type="reset" value="Reset" />
+      <form onSubmit={handleSubmit}>
+        <div className="form-checkboxes">
+          <div className="checkbox-container">
+            <label className="form-label" htmlFor="fir">
+              Fairway
+            </label>
+            <input
+              id="fir"
+              name="fir"
+              type="checkbox"
+              disabled={
+                courses.current.course.parPerHole[currentHole - 1] === 3
+              }
+              onChange={handleBoolChange}
+              checked={courses.current.currentData.fir[currentHole - 1]} // true/false
+            />
+          </div>
+
+          <div className="checkbox-container">
+            <label className="form-label" htmlFor="gir">
+              Green
+            </label>
+            <input
+              id="gir"
+              name="gir"
+              type="checkbox"
+              onChange={handleBoolChange}
+              checked={courses.current.currentData.gir[currentHole - 1]}
+            />
+          </div>
+        </div>
+
+        <div className="hole-navigation">
+          {/* {!previousDisabled && ( */}
+          <button
+            className="hole-nav-buttons"
+            onClick={() => handleClick(-1)}
+            disabled={previousDisabled}
+          >
+            <FaArrowLeft />
+          </button>
+          {/* )} */}
+          <div className="hole-information">
+            <h2>Hole: {currentHole}</h2>
+            <h3>Par: {courses.current.course.parPerHole[currentHole - 1]}</h3>
+          </div>
+          <button
+            className="hole-nav-buttons"
+            onClick={() => handleClick(1)}
+            disabled={nextDisabled}
+          >
+            <FaArrowRight />
+          </button>
+          {/* {!nextDisabled && <FaArrowRight onClick={() => handleClick(1)} />} */}
+        </div>
+
+        <div className="form-number-inputs">
+          <div className="input-container">
+            <label className="form-label" htmlFor="gross">
+              Gross
+            </label>
+            <input
+              id="gross"
+              name="gross"
+              type="number"
+              onChange={handleNumChange}
+              value={courses.current.currentData.gross[currentHole - 1]}
+            />
+          </div>
+
+          <div className="input-container">
+            <label className="form-label" htmlFor="putts">
+              Putts
+            </label>
+            <input
+              id="putts"
+              name="putts"
+              type="number"
+              onChange={handleNumChange}
+              value={courses.current.currentData.putts[currentHole - 1]}
+            />
+          </div>
+        </div>
+
+        <div className="end-button-container">
+          <button className="end-button reset-button" type="reset">
+            Reset
+          </button>
+          <button
+            className={
+              currentHole === holeLength
+                ? 'end-button submit-button'
+                : 'end-button submit-button-disabled'
+            }
+            type="submit"
+            disabled={!(currentHole === holeLength)}
+          >
+            Submit
+          </button>
+        </div>
       </form>
     </>
   )
