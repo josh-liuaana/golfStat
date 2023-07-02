9d444785500389dcde48c44d8a8a0ccca2c19c21
Josh Liuaana <jfliuaana@gmail.com>
2023-07-03 10:53:03 +1200

    rounds styling

--- client/components/Rounds.tsx
+++ client/components/Rounds.tsx
@@ -1,11 +1,11 @@
-import { NavLink } from "react-router-dom"
-import { FERound } from "../../models/types"
-import { useAppSelector, useAppDispatch } from "../hooks/redux"
-import * as actions from "../actions/rounds"
+import { NavLink } from 'react-router-dom'
+import { FERound } from '../../models/types'
+import { useAppSelector, useAppDispatch } from '../hooks/redux'
+import * as actions from '../actions/rounds'
 
 function Rounds() {
   const dispatch = useAppDispatch()
-  const rounds = useAppSelector(state => state.rounds) as FERound[]
+  const rounds = useAppSelector((state) => state.rounds) as FERound[]
 
   const handleClick = (id: number) => {
     dispatch(actions.getSingleRound(id))
@@ -13,14 +13,21 @@ function Rounds() {
 
   return (
     <>
-      <p>Rounds Component</p>
-      {rounds.map((round) => (
-        <div key={round.id}>
-          <NavLink to={`/round/${round.id}`}>
-            <button onClick={() => handleClick(round.id)}>{round.golferName} @ {round.courseName} - {round.createdAt}</button>
-          </NavLink>
-        </div>
-      ))}
+      <h2 className="previous-rounds-title">Previous Rounds</h2>
+      <div className="round-container">
+        {rounds.map((round) => (
+          <div key={round.id}>
+            <NavLink to={`/round/${round.id}`}>
+              <button
+                className="round-button"
+                onClick={() => handleClick(round.id)}
+              >
+                {round.createdAt} - {round.courseName}
+              </button>
+            </NavLink>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
