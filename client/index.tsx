2d770377212edd434de0d62aab0109fe47931180
Josh Liuaana <jfliuaana@gmail.com>
2023-07-13 22:02:57 +1200

    implement react-modal

--- client/index.tsx
+++ client/index.tsx
@@ -1,11 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
+import Modal from 'react-modal'
 
 import store from './store'
 import router from './router'
 
 import './styles/index.scss'
+
+Modal.setAppElement(document.getElementById('app') as HTMLElement)
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
